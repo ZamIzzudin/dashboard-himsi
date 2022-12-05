3d39611eeb7fa478b584fa320fa2ecac5d8597bc
Wildan <wildannurrahman30@gmail.com>
2022-12-05 22:33:48 +0700

    Revert "Event, Article Raw Tabs"
    This reverts commit 56d44c7a46867c96f33bc0a6cb431e53a2916794.

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,30 +1,7 @@
-import { useState } from "react";
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import Form from "../components/Form";
-import List from "../components/List";
-
-
-export default function Event() {
-
-  const [key, setKey] = useState('create');
-
+export default function Article() {
   return (
     <main>
-        <Tabs
-        id="event-tab"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="d-flex flex-row gap-0 my-5 fs-6 fw-normal"
-        >
-          <Tab i="bi bi-list" eventKey='create' title="Create">
-            <Form />
-          </Tab>
-          <Tab eventKey='list' title="List">
-            <List />
-          </Tab>
-
-        </Tabs>
+      <h1>Article</h1>
     </main>
   );
 }
