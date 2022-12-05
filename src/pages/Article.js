56d44c7a46867c96f33bc0a6cb431e53a2916794
Wildan <wildannurrahman30@gmail.com>
2022-12-05 22:16:52 +0700

    Event, Article Raw Tabs

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,30 @@
-export default function Article() {
+import { useState } from "react";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Form from "../components/Form";
+import List from "../components/List";
+
+
+export default function Event() {
+
+  const [key, setKey] = useState('create');
+
   return (
     <main>
-      <h1>Article</h1>
+        <Tabs
+        id="event-tab"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="d-flex flex-row gap-0 my-5 fs-6 fw-normal"
+        >
+          <Tab i="bi bi-list" eventKey='create' title="Create">
+            <Form />
+          </Tab>
+          <Tab eventKey='list' title="List">
+            <List />
+          </Tab>
+
+        </Tabs>
     </main>
   );
 }
