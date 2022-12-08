bd7bb33c2366e2b6268ad6765928f7c796241493
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-12-08 12:12:00 +0700

    merge with branch event and article

---
+++ src/components/EventList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import dummy from "../assets/DummyData";
+
+const Event = () => {
+
+    return (
+        <div className="mx-3">
+            {dummy.map((data, index) => {
+                return (
+                    <div key={index} >
+                        <p>Data ke- {index}</p>
+                        <p>{data.Title}</p>
+                        <p>{data.Category}</p>
+                        <p>{data.Body}</p>
+                        <br />
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Event;
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,31 @@
 import BackButton from '../components/BackButton'
+import { useState } from "react";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import ArticleForm from "../components/ArticleForm";
+import ArticleList from "../components/ArticleList";
 
 export default function Article() {
+
+  const [key, setKey] = useState('create');
+
   return (
     <main>
       <BackButton />
-      <h1>Article</h1>
+      <Tabs
+        id="event-tab"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="d-flex flex-row gap-0 my-5 fs-6 fw-normal"
+      >
+        <Tab i="bi bi-list" eventKey='create' title="Create">
+          <ArticleForm />
+        </Tab>
+        <Tab eventKey='list' title="List">
+          <ArticleList />
+        </Tab>
+
+      </Tabs>
     </main>
   );
 }
